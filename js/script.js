772fb7d8bc929d72c799fd3744c47fe0ecd1ab2b
nazmariam <fedirko.mary@gmail.com>
2019-08-01 20:09:16 +0300

    running numbers

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// бегущие цифры по скролу -------
+var scroller = true;
+$(window).scroll(function () {
+    if (scroller===true) {
+        if( $(window).scrollTop() > 300 ) {
+            $({ n: 1 }).animate({ n: 25 }, {
+                duration: 3000,
+                step: function (a) {
+                    $(".ben_years").html(a | 0)
+                }
+            });
+            $({ n: 1 }).animate({ n: 20 }, {
+                duration: 3000,
+                step: function (a) {
+                    $(".ben_time").html(a | 0)
+                }
+            });
+            $({ n: 1 }).animate({ n: 17 }, {
+                duration: 3000,
+                step: function (a) {
+                    $(".ben_parts").html(a | 0)
+                }
+            });
+            $({ n: 1 }).animate({ n: 5587 }, {
+                duration: 3000,
+                step: function (a) {
+                    $(".ben_tickets").html(a | 0)
+                }
+            });
+            scroller = false;
+        }
+    }
+})
+// бегущие цифры по скролу end ------------
